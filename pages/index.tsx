04f27d822dcd1f877950db5d723ce857d04bca89
Tramy Nguyen <tramynn.dev@gmail.com>
2023-12-29 14:33:53 -0600

    Removes date

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Coming Soon<br />Summer 2022
+          Coming Soon<br />
         </p>
       </main>
     </div>
