fdaa047b85a8eb5e9b59690edaddcee9acd62106
Tramy Nguyen <tramynn.dev@gmail.com>
2024-02-05 17:18:55 -0600

    Add disabled rule

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 
 // eslint-disable-next-line react/jsx-props-no-spreading
 const MyApp = ({ Component, pageProps }: AppProps) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
   <Component {...pageProps} />
 );
 
