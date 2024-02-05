cd452bb83b58f64110d5480f891b503b51d87e67
Tramy Nguyen <tramynn.dev@gmail.com>
2024-02-05 17:33:58 -0600

    lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 import "../styles/reset.css";
 import type { AppProps } from "next/app";
 
-// eslint-disable-next-line react/jsx-props-no-spreading
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
   <Component {...pageProps} />
 );
 
