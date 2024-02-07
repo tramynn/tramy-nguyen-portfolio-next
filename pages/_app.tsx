a44fe7b0bbba438dc0a21f84046432aa28be56d0
Tramy Nguyen <tramynn.dev@gmail.com>
2024-02-07 17:48:09 -0600

    Installs Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../shared/styles/reset.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react';
 import theme from '../shared/styles/theme';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
 	<ChakraProvider theme={theme}>
 		<Component {...pageProps} />
+		<Analytics />
 	</ChakraProvider>
 );
 
