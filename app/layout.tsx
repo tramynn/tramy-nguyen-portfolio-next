9531c7664b07fbe1a79ca58cf35ac005e2f80103
Tramy Nguyen <tramynn.dev@gmail.com>
2024-02-13 17:11:54 -0600

    Adds chakra ui color mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import { Metadata } from 'next';
 import React from 'react';
 import '../shared/styles/reset.css';
 import { Analytics } from '@vercel/analytics/react';
+import { ColorModeScript } from '@chakra-ui/react';
 import fonts from '../shared/fonts';
 import Providers from './providers';
+import theme from '../shared/styles/theme';
 
 export const metadata: Metadata = {
 	title: 'Tra My Ngoc Nguyen',
@@ -19,6 +21,7 @@ const RootLayout = ({
 }) => (
 	<html lang='en' className={fonts.raleway.className}>
 		<body>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 			<Providers>{children}</Providers>
 			<Analytics />
 		</body>
